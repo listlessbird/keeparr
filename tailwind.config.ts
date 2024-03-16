63daf7d511f2d965ef059a80dd28e8543937f897
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-03-17 01:17:18 +0530

    feat: use clamp for fontsizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,17 @@ module.exports = {
       },
     },
     extend: {
+      fontSize: {
+        "--font-size-sm": "clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem)",
+        "--font-size-base": "clamp(1rem, 0.34vw + 0.91rem, 1.19rem)",
+        "--font-size-lg": "clamp(1.25rem, 0.61vw + 1.1rem, 1.58rem)",
+        "--font-size-xl": "clamp(1.56rem, 1vw + 1.31rem, 2.11rem)",
+        "--font-size-2xl": "clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)",
+        "--font-size-3xl": "clamp(2.44rem, 2.38vw + 1.85rem, 3.75rem)",
+        "--font-size-4xl": "clamp(3.05rem, 3.54vw + 2.17rem, 5rem)",
+        "--font-size-5xl": "clamp(3.81rem, 5.18vw + 2.52rem, 6.66rem)",
+        "--font-size-6xl": "clamp(4.77rem, 7.48vw + 2.9rem, 8.88rem)",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
