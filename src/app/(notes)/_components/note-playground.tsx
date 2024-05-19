ef70a0fe911cbc53bda92af57acadf13ee6c5d0d
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-05-19 11:17:09 +0530

    fix: contain the height of the block note editor

--- src/app/(notes)/_components/note-playground.tsx
+++ src/app/(notes)/_components/note-playground.tsx
@@ -23,11 +23,11 @@ export function NotePlayGround({ initialContent }: NotesPlayGroundProps) {
   const editor = useCreateBlockNote({ initialContent })
 
   return (
-    <div className="flex-auto overflow-auto">
+    <div className="flex-auto">
       <BlockNoteView
         editor={editor}
         theme={playGroundTheme}
-        className="max-w-screen relative p-2 md:p-4"
+        className="max-w-screen relative max-h-[calc(100vh-4rem)] overflow-y-auto p-2 md:p-4"
       />
     </div>
   )
