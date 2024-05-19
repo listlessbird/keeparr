78f8779633038072fc49c58fbfd5fb2c75eeeac0
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-05-19 12:43:31 +0530

    fix: visual glitch when collapsing

--- src/app/(notes)/_components/sidebar.tsx
+++ src/app/(notes)/_components/sidebar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { motion } from "framer-motion"
 import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
@@ -18,44 +19,59 @@ export function NotesSidebar({ children }: { children: React.ReactNode }) {
   const { isExpanded, setIsExpanded } = useNotesLayoutState()
 
   return (
-    <div
-      // className="w-[250px]"
-      className={isExpanded ? "w-[250px]" : "w-0"}
+    <motion.div
+      className={cn(
+        `ease-[cubic-bezier(0.165,0.84,0.44,1)] z-50 flex shrink-0 flex-col bg-[#f0f0d5] text-black transition-transform duration-300`,
+        // isExpanded ? "w-full" : "invisible"
+        isExpanded ? "translate-x-0" : "-translate-x-full",
+      )}
+      initial={{ width: isExpanded ? "250px" : "0px" }}
+      animate={{
+        width: isExpanded ? "250px" : "0px",
+        visibility: isExpanded ? "visible" : "hidden",
+      }}
     >
-      <div
-        className={cn(
-          `fixed inset-y-0 z-50 flex shrink flex-col bg-[#f0f0d5] text-black transition-all duration-300`,
-          // isExpanded ? "w-full" : "invisible"
-          isExpanded ? "translate-x-0" : "-translate-x-full",
-        )}
-      >
-        {isMobile && isExpanded && (
-          <div className="fixed right-2 top-2">
-            <Button
-              variant={"icon"}
-              onClick={() => {
-                setTimeout(() => {
-                  setIsExpanded((prev) => !prev)
-                }, 100)
-              }}
-            >
-              <X size={24} />
-            </Button>
-          </div>
-        )}
-        <div className="flex flex-col p-2">
-          <div className="flex items-center gap-x-2">
-            <UserAvatar />
-            <div className="flex flex-col">
-              <p className="line-clamp-1 text-lg font-bold">{user?.username}</p>
-              <p className="line-clamp-1 text-xs text-gray-400">
-                {user?.email}
-              </p>
-            </div>
-          </div>
-          <div className="mt-2 p-2">{children}</div>
+      {isMobile && isExpanded && (
+        <div className="fixed right-2 top-2">
+          <Button
+            variant={"icon"}
+            onClick={() => {
+              setTimeout(() => {
+                setIsExpanded((prev) => !prev)
+              }, 100)
+            }}
+          >
+            <X size={24} />
+          </Button>
         </div>
-      </div>
-    </div>
+      )}
+      <motion.div
+        className="flex flex-col p-2"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: isExpanded ? 1 : 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <motion.div
+          className="flex items-center gap-x-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isExpanded ? 1 : 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <UserAvatar />
+          <div className="flex flex-col">
+            <p className="line-clamp-1 text-lg font-bold">{user?.username}</p>
+            <p className="line-clamp-1 text-xs text-gray-400">{user?.email}</p>
+          </div>
+        </motion.div>
+        <motion.div
+          className="mt-2 p-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isExpanded ? 1 : 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          {children}
+        </motion.div>
+      </motion.div>
+    </motion.div>
   )
 }
